6dc4510b3ae15e2af426b27cb76cb0f252e8c587
ksenon <apatacheck@mail.ru>
2025-11-22 00:17:35 +0400

    task7

---
+++ src/main/java/org/kniit/lab4/task7/DictionaryStatistic.java
@@ -0,0 +1,86 @@
+package org.kniit.lab4.task7;
+
+import java.util.Random;
+
+class DictionaryStatistic {
+    private String[] words;
+    private int dictionarySize; // Количество слов
+    private int polindrom; // Количество слов полиндромов
+    private int maxWordLength; // максимальная длина слова в словаре
+    private int minWordLength; // минимальная длина слова в словаре
+    private char[] alphabet; // буквы алфавита
+    private int[] frequency; // частота букв
+
+    private Random random = new Random();
+
+    public DictionaryStatistic(String[] words, char[] alphabet) {
+        this.words = words;
+        this.alphabet = alphabet;
+        this.frequency = new int[alphabet.length];
+
+        func();
+    }
+
+    private void func() {
+        dictionarySize = words.length;
+        maxWordLength = 0;
+        minWordLength = 100;
+
+        for (String word : words) {
+            String w = word.toLowerCase();
+            int len = w.length();
+        //длина слов
+            if (len > maxWordLength) maxWordLength = len;
+            if (len < minWordLength) minWordLength = len;
+            //палиндромы
+            if (isPalindrome(w)) {
+                polindrom++;
+            }
+
+            for (char c : w.toCharArray()) {
+                int index = getAlphabetIndex(c);
+                if (index != -1) {
+                    frequency[index]++;
+                }
+            }
+        }
+    }
+
+    private boolean isPalindrome(String w)
+    {
+        return w.equals(new StringBuilder(w).reverse().toString());
+    }
+
+    private int getAlphabetIndex(char c)
+    {
+        for (int i = 0; i < alphabet.length; i++)
+        {
+            if (alphabet[i] == c) return i;
+        }
+        return -1;
+    }
+
+    private String getSymbolsStatString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < alphabet.length; i++) {
+            sb.append(alphabet[i]).append(" - ").append(frequency[i]).append("\n");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Кол-во слов: " + dictionarySize + "\n" +
+                "Палиндромы: " + polindrom + "\n" +
+                "Минимальная длина: " + minWordLength + "\n" +
+                "Максимальная длина: " + maxWordLength + "\n" +
+                "Частота:\n" + getSymbolsStatString();
+    }
+
+    public String getRandomWord()
+    {
+        return words[random.nextInt(words.length)];
+    }
+
+}
---
+++ src/main/java/org/kniit/lab4/task7/Main.java
@@ -0,0 +1,23 @@
+package org.kniit.lab4.task7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("misc/dictionary.txt"));
+        ArrayList<String> list = new ArrayList<>();
+        while (scanner.hasNext()) {
+            list.add(scanner.nextLine().trim());
+        }
+        scanner.close();
+        String[] words = list.toArray(new String[0]); // переводим в массив
+        char[] alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".toCharArray();
+        DictionaryStatistic stat = new DictionaryStatistic(words, alphabet);
+        System.out.println(stat);
+        System.out.println("\nСлучайное слово: " + stat.getRandomWord());
+    }
+}
+
